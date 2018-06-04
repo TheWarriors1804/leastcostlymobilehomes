6a5c978ffaf9775c7f2ca91a86826cfb0dc7311d
Jenny Wang <jennysihua@gmail.com>
2018-06-04 16:12:22 -0400

    add product model definition

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -3,7 +3,7 @@ const db = require('../db')
 
 const Product = db.define('product', {
   price: {
-    type: Sequelize.STRING,
+    type: Sequelize.DECIMAL(10, 2),
     allowNull: false,
   },
   imageUrl: {
@@ -13,16 +13,30 @@ const Product = db.define('product', {
     type: Sequelize.STRING,
   },
   length: {
-
+    type: Sequelize.DECIMAL(10, 2),
   },
   width: {
-
+    type: Sequelize.DECIMAL(10, 2),
   },
   height: {
-
+    type: Sequelize.DECIMAL(10, 2),
   },
   type: {
-
+    type: Sequelize.STRING,
   },
-
+  description: {
+    type: Sequelize.TEXT,
+  },
+  year: {
+    type: Sequelize.INTEGER(4)
+  },
+  bedrooms: {
+    type: Sequelize.DECIMAL(10, 1),
+  },
+  bathrooms: {
+    type: Sequelize.DECIMAL(10, 1),
+  },
+  manufacturer: {
+    type: Sequelize.STRING,
+  }
 })
