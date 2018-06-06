5941d8e0f56609e9c9ccba84945bc7150a698c30
Jenny Wang <jennysihua@gmail.com>
2018-06-06 17:59:43 -0400

    add users and products reducers to combinereducers

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,10 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import order from './order'
+import product from './product'
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({user, order, product})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
