75f359adc552d91c4e51afed39affb255c85b233
Anton Holmes <antonholmes@Antons-MacBook-Pro.local>
2018-06-06 17:04:33 -0400

    (feat) redux commit redone

--- client/store/user.js
+++ client/store/user.js
@@ -1,63 +1,78 @@
-import axios from 'axios'
-import history from '../history'
+import axios from 'axios';
+import history from '../history';
 
 /**
  * ACTION TYPES
  */
-const GET_USER = 'GET_USER'
-const REMOVE_USER = 'REMOVE_USER'
+const GET_USER = 'GET_USER';
+const REMOVE_USER = 'REMOVE_USER';
+const UPDATE_USER = 'UPDATE_USER';
 
 /**
  * INITIAL STATE
  */
-const defaultUser = {}
+const defaultUser = {};
 
 /**
  * ACTION CREATORS
  */
-const getUser = user => ({type: GET_USER, user})
-const removeUser = () => ({type: REMOVE_USER})
+const getUser = user => ({ type: GET_USER, user });
+const removeUser = () => ({ type: REMOVE_USER });
+const updatedUser = user => ({ type: UPDATE_USER, user });
 
 /**
  * THUNK CREATORS
  */
-export const me = () =>
-  dispatch =>
-    axios.get('/auth/me')
-      .then(res =>
-        dispatch(getUser(res.data || defaultUser)))
-      .catch(err => console.log(err))
+export const me = () => dispatch =>
+  axios
+    .get('/auth/me')
+    .then(res => dispatch(getUser(res.data || defaultUser)))
+    .catch(err => console.log(err));
 
-export const auth = (email, password, method) =>
-  dispatch =>
-    axios.post(`/auth/${method}`, { email, password })
-      .then(res => {
-        dispatch(getUser(res.data))
-        history.push('/home')
-      }, authError => { // rare example: a good use case for parallel (non-catch) error handler
-        dispatch(getUser({error: authError}))
-      })
-      .catch(dispatchOrHistoryErr => console.error(dispatchOrHistoryErr))
+export const auth = (email, password, method) => dispatch =>
+  axios
+    .post(`/auth/${method}`, { email, password })
+    .then(
+      res => {
+        dispatch(getUser(res.data));
+        history.push('/home');
+      },
+      authError => {
+        // rare example: a good use case for parallel (non-catch) error handler
+        dispatch(getUser({ error: authError }));
+      }
+    )
+    .catch(dispatchOrHistoryErr => console.error(dispatchOrHistoryErr));
 
-export const logout = () =>
-  dispatch =>
-    axios.post('/auth/logout')
-      .then(_ => {
-        dispatch(removeUser())
-        history.push('/login')
-      })
-      .catch(err => console.log(err))
+export const logout = () => dispatch =>
+  axios
+    .post('/auth/logout')
+    .then(_ => {
+      dispatch(removeUser());
+      history.push('/login');
+    })
+    .catch(err => console.log(err));
+
+export const updateUser = user => async dispatch => {
+  const updated = await axios.put(`/api/users`, user);
+  dispatch(updatedUser(updated));
+};
+
+export const deleteUser = id => async dispatch => {
+  await axios.delete(`/api/users/${id}`);
+  dispatch(removeUser());
+};
 
 /**
  * REDUCER
  */
-export default function (state = defaultUser, action) {
+export default function(state = defaultUser, action) {
   switch (action.type) {
     case GET_USER:
-      return action.user
+      return action.user;
     case REMOVE_USER:
-      return defaultUser
+      return defaultUser;
     default:
-      return state
+      return state;
   }
 }
