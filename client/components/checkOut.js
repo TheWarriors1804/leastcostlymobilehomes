13f2fa95d4920b5c007a995009f12db616647991
Jesse Sullivan <11592718+Delune@users.noreply.github.com>
2018-06-07 15:13:59 -0400

    feat(checkout) new component

---
+++ client/components/checkOut.js
@@ -0,0 +1,17 @@
+import { Link } from 'react-dom';
+import React from 'react';
+import { HomeSearchCard } from './index';
+
+const CheckOut = props => {
+  return (
+    <div>
+      <div>CheckOut!!!</div>
+      <hr />
+      <div>
+        <HomeSearchCard />
+      </div>
+    </div>
+  );
+};
+
+export default CheckOut;
