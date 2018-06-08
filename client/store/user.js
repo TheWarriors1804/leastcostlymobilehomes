3629e9ffa8a75cc0600d8c0182af0f988e0e5c72
Teresa Li <teresa@Teresas-MacBook-Pro.local>
2018-06-08 15:31:09 -0400

    dummy variable added

--- client/store/user.js
+++ client/store/user.js
@@ -31,12 +31,10 @@ export const me = () => dispatch =>
     .catch(err => console.log(err))
 
 //thunk for login
-export const auth = (email, password, method, firstName, lastName) => {
-  const cart = localStorage
-  // console.log('localStorage', localStorage)
+export const auth = (email, password, method, cart, firstName, lastName) => {
   return dispatch =>
     axios
-      .post(`/auth/${method}`, {email, password})
+      .post(`/auth/${method}`, {email, password, cart})
       .then(
         res => {
           dispatch(getUser(res.data))
