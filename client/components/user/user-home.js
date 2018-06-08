a6038295fdca86f354315044510c4467c7ccb885
Anton Holmes <antonholmes@Antons-MacBook-Pro.local>
2018-06-08 16:34:34 -0400

    feat(profile page): WIP previous orders, integrated with user details

--- client/components/user/user-home.js
+++ client/components/user/user-home.js
@@ -13,22 +13,22 @@ export const UserHome = props => {
     <div>
       <h3>
         Welcome, {firstName} {lastName}!
-        <div className="row">
-          <div className="card horizontal col s11 m11 l11">
-            <div className="card-image">
-              <img src={imageUrl} />
-            </div>
-            <div className="card-content">
-              <UserInfo />
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="card horizontal col s10 m10 l10">
-            <h3> Previous Orders </h3>
-          </div>
-        </div>
       </h3>
+      <div className="row">
+        <div className="card horizontal col s11 m11 l11">
+          <div className="card-image">
+            <img src={imageUrl} />
+          </div>
+          <div className="card-content">
+            <UserInfo />
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="card horizontal col s10 m10 l10">
+          <h2> Previous Orders </h2>
+        </div>
+      </div>
     </div>
   )
 }
