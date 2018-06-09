bd3aa500359695a6797ea59da5824109602fbad0
Anton Holmes <antonholmes@Antons-MacBook-Pro.local>
2018-06-09 16:46:25 -0400

    wip to pull master

--- client/components/user/user-order.js
+++ client/components/user/user-order.js
@@ -1,51 +1,71 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
-import {UserInfo} from '../index'
+import {HomeTitle, HomeInfo, HomeContent} from '../index'
 
 /**
  * COMPONENT
  */
 export const UserOrder = props => {
-  const {firstName, lastName, imageUrl} = props
+  const handleSubmit = () => {}
+  const current = props.products
+    ? props.products.find(product => {
+        return product.id === +props.match.params.id
+      })
+    : []
+  if (current) {
+    return (
+      <div>
+        <HomeTitle info={current} />
+        <div className="flexContainer">
+          <img src={current.imageUrl} className="homeImage" />
+          <HomeInfo info={current} handleSubmit={handleSubmit} />
+        </div>
+        <HomeContent info={current} />
+      </div>
+    )
+  } else {
+    return <div />
+  }
+  // const {imageUrl, productId, quantity} = props
 
-  return (
-    <div>
-      <div className="row">
-        <div className="card horizontal col s11 m11 l11">
-          <div className="card-image">
-            <img src={imageUrl} />
-          </div>
-          <div className="card-content">{/* <UserInfo /> */}</div>
-        </div>
-      </div>
-      <div className="row">
-        <div className="card horizontal col s10 m10 l10">
-          <h2> Previous Orders </h2>
-        </div>
-      </div>
-    </div>
-  )
+  // return (
+  //   <div>
+  //     <div className="row">
+  //       <div className="card horizontal col s11 m11 l11">
+  //         <div className="card-image">
+  //           <img src={imageUrl} />
+  //         </div>
+  //         <div className="card-content">{/* <UserInfo /> */}</div>
+  //       </div>
+  //     </div>
+  //     <div className="row">
+  //       <div className="card horizontal col s10 m10 l10">
+  //         <h2> Previous Orders </h2>
+  //       </div>
+  //     </div>
+  //   </div>
+  // )
 }
 
 /**
  * CONTAINER
  */
-const mapState = state => {
+const mapStateToProps = state => {
   return {
-    firstName: state.user.firstName,
-    lastName: state.user.lastName,
-    imageUrl: state.user.imageUrl
+    userId: state.user.userId,
+    productId: state.user.productId,
+    quantity: state.user.quantity
   }
 }
 
-export default connect(mapState)(UserOrder)
+export default connect(mapStateToProps)(UserOrder)
 
 /**
  * PROP TYPES
  */
 UserOrder.propTypes = {
-  firstName: PropTypes.string,
-  lastName: PropTypes.string,
-  imageUrl: PropTypes.string
+  userId: PropTypes.string,
+  productId: PropTypes.string,
+  quantity: PropTypes.string
 }
