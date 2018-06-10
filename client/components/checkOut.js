e5cb5a3af077b9fe6b70c54d4e9ed464a25fe6f5
Jenny Wang <jennysihua@gmail.com>
2018-06-10 15:14:51 -0400

    Initial checkout commit

--- client/components/checkOut.js
+++ client/components/checkOut.js
@@ -1,17 +1,43 @@
 import {Link} from 'react-dom'
 import React from 'react'
 import {HomeSearchCard} from './index'
+import {connect} from 'react-redux'
 
 const CheckOut = props => {
+  console.log('order', props.order)
+  console.log('products', props.products)
   return (
     <div>
-      <div>CheckOut!!!</div>
-      <hr />
-      <div>
-        <HomeSearchCard />
-      </div>
+      <h1>Shopping Cart</h1>
+      <h2>
+        You have{' '}
+        {Object.keys(props.order).reduce(
+          (acc, curr) => acc + props.order[curr],
+          0
+        )}{' '}
+        items in your shopping cart.
+      </h2>
+      {Object.keys(props.order).length && props.products.length ? (
+        Object.keys(props.order).map(productId => (
+          <HomeSearchCard
+            product={props.products.find(
+              product => product.id === Number(productId)
+            )}
+            key={productId}
+            quantity={props.order[productId]}
+          />
+        ))
+      ) : (
+        <div />
+      )}
     </div>
   )
 }
 
-export default CheckOut
+const mapStateToProps = state => ({
+  // order: state.order,
+  order: {'2': 1, '3': 2},
+  products: state.product
+})
+
+export default connect(mapStateToProps)(CheckOut)
