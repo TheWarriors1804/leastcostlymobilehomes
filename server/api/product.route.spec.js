6203f8aab11871846125e1908e315a6a854f03c2
Anton Holmes <holmesanton@icloud.com>
2018-06-12 12:48:07 -0400

    wip

--- server/api/product.route.spec.js
+++ server/api/product.route.spec.js
@@ -4,56 +4,7 @@ const {expect} = require('chai')
 const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
-const User = db.model('user')
 const Product = db.model('product')
-
-describe('User GET User routes', () => {
-  beforeEach(() => {
-    return db.sync({force: true})
-  })
-
-  describe('/api/users/', () => {
-    const doloresEmail = 'dolores@westworld.com'
-    beforeEach(() => {
-      return User.create({
-        firstName: 'Dolores',
-        lastName: 'Abernathy',
-        email: doloresEmail
-      })
-    })
-
-    it('GET /api/users', () => {
-      return request(app)
-        .get('/api/users')
-        .expect(200)
-        .then(res => {
-          expect(res.body).to.be.an('array')
-          expect(res.body[0].email).to.be.equal(doloresEmail)
-        })
-    })
-  }) // end describe('/api/users')
-
-  describe('/api/users/:id', () => {
-    const teddyEmail = 'teddy@westworld.com'
-    beforeEach(() => {
-      return User.create({
-        firstName: 'Teddy',
-        lastName: 'Flood',
-        email: teddyEmail
-      })
-    })
-
-    it('GET /api/users/:id', () => {
-      return request(app)
-        .get('/api/users/1')
-        .expect(200)
-        .then(res => {
-          expect(res.body).to.be.an('object')
-          expect(res.body.email).to.be.equal(teddyEmail)
-        })
-    })
-  })
-}) // end describe('User routes')
 
 describe('User GET Product Routes', () => {
   beforeEach(() => {
@@ -61,7 +12,7 @@ describe('User GET Product Routes', () => {
   })
 
   describe('GET /api/products', () => {
-    const price1 = 500.99
+    const price1 = 500.0
     beforeEach(() => {
       return Product.create({
         price: price1,
@@ -75,8 +26,7 @@ describe('User GET Product Routes', () => {
         .expect(200)
         .then(res => {
           expect(res.body).to.be.an('array')
-          console.log('console.log', res.body)
-          expect(res.body[0].price).to.be.equal('500.99')
+          expect(res.body[0].price).to.be.equal('500.00')
         })
     })
   })
@@ -88,7 +38,10 @@ describe('User GET Product Routes', () => {
           price: 99.99,
           model: 'X01'
         },
-        {price: 100.5, model: 'X02'}
+        {
+          price: 100.5,
+          model: 'X02'
+        }
       ])
     })
 
--- server/api/user.spec.js
+++ server/api/user.spec.js
@@ -5,7 +5,6 @@ const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
-const Product = db.model('product')
 
 describe('Initial user tests', () => {
   beforeEach(() => {
@@ -74,4 +73,51 @@ describe('Initial user tests', () => {
         })
     })
   })
+  describe('User GET User routes', () => {
+    beforeEach(() => {
+      return db.sync({force: true})
+    })
+
+    describe('/api/users/', () => {
+      const doloresEmail = 'dolores@westworld.com'
+      beforeEach(() => {
+        return User.create({
+          firstName: 'Dolores',
+          lastName: 'Abernathy',
+          email: doloresEmail
+        })
+      })
+
+      it('GET /api/users', () => {
+        return request(app)
+          .get('/api/users')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('array')
+            expect(res.body[0].email).to.be.equal(doloresEmail)
+          })
+      })
+    }) // end describe('/api/users')
+
+    describe('/api/users/:id', () => {
+      const teddyEmail = 'teddy@westworld.com'
+      beforeEach(() => {
+        return User.create({
+          firstName: 'Teddy',
+          lastName: 'Flood',
+          email: teddyEmail
+        })
+      })
+
+      it('GET /api/users/:id', () => {
+        return request(app)
+          .get('/api/users/1')
+          .expect(200)
+          .then(res => {
+            expect(res.body).to.be.an('object')
+            expect(res.body.email).to.be.equal(teddyEmail)
+          })
+      })
+    })
+  }) // end describe('User routes')
 })
